25c62d090cfc3f0827daf2e7affa87c1f3f0679d
Steve2a <saagyemang014@st.ug.edu.gh>
2025-08-05 11:50:13 +0000

    gradecalculator completed

--- GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
+++ GradeCalculator/obj/Debug/net9.0/GradeCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5d9fd3898868d6b97e40ed75bc09f28ce679d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
